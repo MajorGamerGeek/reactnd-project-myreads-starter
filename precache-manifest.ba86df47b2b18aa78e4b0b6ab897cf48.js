dd2c116d47a30091f55a9954d6afc14895d4dd31
MajorGamerGeek <amaloy@gmail.com>
2020-07-20 00:19:33 -0700

    Updates

---
+++ precache-manifest.ba86df47b2b18aa78e4b0b6ab897cf48.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e55e4e948e07fd3a70cba1e525ae39",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  },
+  {
+    "revision": "740e1d348de94070ff3c",
+    "url": "/reactnd-project-myreads-starter/static/css/main.305ceecc.chunk.css"
+  },
+  {
+    "revision": "b5d341d46e340ca07f3e",
+    "url": "/reactnd-project-myreads-starter/static/js/2.7025fafa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactnd-project-myreads-starter/static/js/2.7025fafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740e1d348de94070ff3c",
+    "url": "/reactnd-project-myreads-starter/static/js/main.d698277c.chunk.js"
+  },
+  {
+    "revision": "a4b435ad011df39f0faf",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime-main.cc6fa4a5.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "9b517014bd671e6535841ee33cfda677",
+    "url": "/reactnd-project-myreads-starter/static/media/search.9b517014.svg"
+  }
+]);
